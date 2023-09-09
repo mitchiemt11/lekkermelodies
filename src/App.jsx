603de8089f7b4a37786ae49a1b82156cd7bae613
Mitchell <mitchiemt11@gmail.com>
2023-09-09 16:40:49 +0200

    Make about section a standalone

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Landing from './sections/Landing';
-import Blog from './sections/Blog';
-import About from './sections/About';
+
 
 function App() {
   return (
     <>
       <Landing/>
-      <About/>
     </>
   );
 }
