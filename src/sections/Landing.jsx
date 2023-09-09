a55b2598c0b26568365579dcf09eabd8fec714ba
Mitchell <mitchiemt11@gmail.com>
2023-09-09 03:33:05 +0200

    Add a simple header

--- src/sections/Landing.jsx
+++ src/sections/Landing.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div></div>
+    <div className='min-h-screen flex flex-col text-white'>
+			<header className='container mx-auto p-6 flex flex-col md:flex-row items-center justify-between'>
+        <div className='flex -mx-6'>
+          <a href="#" className="mx-3 hover:opacity-80 duration duration-150">Overview</a> |
+          <a href="#" className="mx-3 hover:opacity-80 duration duration-150">About</a> |
+          <a href="#" className="mx-3 hover:opacity-80 duration duration-150">Blog</a>
+        </div>
+      </header>
+    </div>
   )
 }
 
