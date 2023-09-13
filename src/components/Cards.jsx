60eaefc31323a5ee8bb9ce8a9a8f88ba9087c755
Mitchell <mitchiemt11@gmail.com>
2023-09-14 00:37:48 +0200

    Add controls to the card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PlayIcon from './PlayIcon';
+import VolumeIcon from './VolumeIcon';
 
 const Cards = () => {
   return (
@@ -6,9 +8,16 @@ const Cards = () => {
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
       </div>
-      <div className="px-6 pt-4 pb-2 justify-between">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+      <div className="px-6 pt-4 pb-2 flex items-center justify-between">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          <PlayIcon/>
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        <div className="flex items-center justify-center">
+            <input type='range'/>
+          </div>
+        </span>
+        
       </div>
     </div>
   );
