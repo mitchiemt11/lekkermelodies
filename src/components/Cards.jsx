7dd1084e102e2fac77258056f074ae40bc5fcf0b
Mitchell <mitchiemt11@gmail.com>
2023-09-13 23:16:56 +0200

    Make a minimalist design for cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 const Cards = () => {
   return (
-    <div class="max-w-xs rounded-lg bg-gray-400 overflow-hidden shadow-lg">
-      <img class="w-full" src="https://picsum.photos/600/400/?random" alt="Sunset in the mountains"/>
-    <div class="px-6 py-4">
-      <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <div className="max-w-xs rounded-lg bg-gray-400 overflow-hidden shadow-lg" style={{ boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }}>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+      </div>
+      <div className="px-6 pt-4 pb-2 justify-between">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+      </div>
     </div>
-    <div class="px-6 pt-4 pb-2 justify-between">
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    </div>
-  </div>
   );
 }
 
