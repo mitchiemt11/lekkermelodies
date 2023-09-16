f6642f95dbdfbbf217050b089046b81573eac0ab
Mitchell <mitchiemt11@gmail.com>
2023-09-16 03:37:09 +0200

    Match color for play icon

--- src/components/PlayIcon.jsx
+++ src/components/PlayIcon.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PlayIcon = () => {
   return (
     <div>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#0274ff" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#f2f1f4" className="w-12 h-12">
         <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
       </svg>
