12321311b3419534a3ccb09ef090fe6367455d47
Mitchell <mitchiemt11@gmail.com>
2023-09-25 17:34:51 +0200

    Add correct audio source

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,21 +1,50 @@
 import React from 'react';
 import PlayIcon from './PlayIcon';
+import Campfire from '../data/nature_sounds/campfire-crackling.mp3'
+import CalmRiver from '../data/nature_sounds/calm-river.mp3'
+import Chimes from '../data/nature_sounds/chimes-bells.mp3'
+import Eveningbirds from '../data/nature_sounds/evening-birds.mp3'
+import Thunder from '../data/nature_sounds/lighting-thunder.mp3'
+import Nature from '../data/nature_sounds/nature-relax-birds.mp3'
+import Night from '../data/nature_sounds/night-field-cricket.mp3'
+import Rain from '../data/nature_sounds/rain-ambient.mp3'
+import Waves from '../data/nature_sounds/small-waves.mp3'
+import Piano from '../data/nature_sounds/soft-piano.mp3'
+import WhiteNoise from '../data/nature_sounds/white-noise.mp3'
+import Wind from '../data/nature_sounds/wind-outside.mp3'
 
 const Cards = () => {
   const cardData = [
-    { title: 'Evening Birds' },
-    { title: 'Campfire Crackling' },
-    { title: 'Wind Outside' },
-    { title: 'Small Waves' },
-    { title: 'White Noise' },
-    { title: 'Rain Ambient' },
-    { title: 'Nature Relax: Birds' },
-    { title: 'Lightning & Thunder' },
-    { title: 'Chimes Bells' },
-    { title: 'Night Field Cricket' },
-    { title: 'Soft Piano' },
-    { title: 'Calm River' },
+    { title: 'Evening Birds', audioSrc: Eveningbirds },
+    { title: 'Campfire Crackling', audioSrc: Campfire },
+    { title: 'Wind Outside', audioSrc: Wind },
+    { title: 'Small Waves', audioSrc: Waves },
+    { title: 'White Noise', audioSrc: WhiteNoise },
+    { title: 'Rain Ambient', audioSrc: Rain },
+    { title: 'Nature Relax: Birds', audioSrc: Nature },
+    { title: 'Lightning & Thunder', audioSrc: Thunder },
+    { title: 'Chimes Bells', audioSrc: Chimes },
+    { title: 'Night Field Cricket', audioSrc: Night },
+    { title: 'Soft Piano', audioSrc: Piano },
+    { title: 'Calm River', audioSrc: CalmRiver },
   ];
+
+  // const [audioElements, setAudioElements] = React.useState({});
+  const audioElements = React.useRef({});
+
+
+
+  const toggleAudio = (title) => {
+    const audio = audioElements.current[title]; // Retrieve the audio element using the title
+    if (audio && audio.paused) { // Check if audio exists and is paused
+      audio.play();
+    } else if (audio) { // Check if audio exists
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  };
+  
+
 
   return (
     <div className="flex flex-wrap -mx-4">
@@ -26,7 +55,7 @@ const Cards = () => {
               <div className="font-bold text-xl mb-2">{card.title}</div>
             </div>
             <div className="px-6 pt-4 pb-2 flex items-center justify-between">
-              <span className="inline-block cursor-pointer rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              <span onClick={() => toggleAudio(card.title)} className="inline-block cursor-pointer rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                 <PlayIcon />
               </span>
               <span className="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
@@ -35,6 +64,11 @@ const Cards = () => {
                 </div>
               </span>
             </div>
+            <audio ref={(audio) => (audioElements.current[card.title] = audio)}>
+  <source src={card.audioSrc} type="audio/mp3" />
+  Your browser does not support the audio element.
+</audio>
+
           </div>
         </div>
       ))}
