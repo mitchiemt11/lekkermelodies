3b6cf3f2daf424e5e20d6bb19ddcb35b63cdc368
Mitchell <mitchiemt11@gmail.com>
2023-10-14 17:19:39 +0200

    Render actual post data

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { posts } from '../data';
 
 const Post = () => {
+  // Get the "id" parameter from the URL using useParams
+  const { id } = useParams();
 
-  const {id, title} = useParams()
-  console.log(id, title)
+  // Find the post with the matching "id"
+  const post = posts.find((post) => post.id === parseInt(id, 10));
+
+  // Check if a post with the specified "id" was found
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
   return (
     <div className="bg-gray-300 rounded-t-3xl px-6 pt-32 lg:px-8">
       <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
-        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Overview</h1>
-        <h2 className="mt-4 text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">By Mitchell M</h2>
-        <p className="mt-6 text-xl leading-8">
-          Lekker Melodies is all about finding peace and focus through sounds.
-          It's like having your own secret tool to make noisy places quieter. Here's what you can expect:
-        </p>
+        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{post.title}</h1>
+        <p className="mt-6 text-xl leading-8">{post.body}</p>
+        <h2 className="mt-4 text-xl font-bold tracking-tight text-gray-900 ">By {post.author.name}</h2>
         <div className="mt-10 max-w-2xl mb-8">
           <h3 className="mt-16 text-2xl font-bold tracking-tight text-gray-900 mb-8">🎵 🎹🎹🎹 🎹🎹🎹 🎵</h3>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Post;
