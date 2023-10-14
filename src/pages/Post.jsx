868d6eb1629081f0aca8b4f907fd706b7ee5dc14
Mitchell <mitchiemt11@gmail.com>
2023-10-14 16:23:21 +0200

    Setup a central data location

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Post = () => {
+
+  const {id, title} = useParams()
+  console.log(id, title)
   return (
     <div>Post</div>
   )
