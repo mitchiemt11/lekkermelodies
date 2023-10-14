7898c12390a1ee7b529fd86cdef39f4120e2700b
Mitchell <mitchiemt11@gmail.com>
2023-10-14 23:50:25 +0200

    Use a 404 image when post not found

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { posts } from '../data';
+import PANDA_404 from '../data/assets/panda_404.png';
 
 const Post = () => {
   // Get the "id" parameter from the URL using useParams
@@ -11,7 +12,23 @@ const Post = () => {
 
   // Check if a post with the specified "id" was found
   if (!post) {
-    return <div>Post not found</div>;
+    return (
+      <div className="bg-gray-300 rounded-t-3xl px-6 pt-32 lg:px-8">
+        <Link to="/blog" className="text-gray-900 hover:text-gray-600 text-xl absolute top-40 left-20">
+          &larr;  Back
+        </Link>
+        <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700 text-center justify-center items-center">
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">OOPS!</h1>
+          <div  className="flex flex-col items-center justify-center">
+          <img
+            src={PANDA_404}
+            alt="Post not found"
+            className="mt-6"
+          />
+        </div>
+        </div>
+      </div>
+    );
   }
 
   return (
