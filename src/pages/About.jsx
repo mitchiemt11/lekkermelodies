038f9dc24ca6334c83aaf8c9c648812b21d5aa9e
Mitchell <mitchiemt11@gmail.com>
2023-10-15 21:52:59 +0200

    Adjust profile image size

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,8 +13,12 @@ const About = () => {
       <div className="w-screen h-screen flex flex-row flex-wrap p-3">
         <div className="mx-auto w-2/3">
           <div className="rounded-lg shadow-lg bg-gray-600 w-full flex flex-row flex-wrap p-3 antialiased" style={backgroundStyle}>
-            <div className="md:w-1/3 w-full">
-              <img className="rounded-full shadow-lg antialiased" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="User Profile" />
+            <div className="md:w-1/3 w-full flex justify-center mt-28">
+              <img
+                className="rounded-full shadow-lg antialiased w-44 h-44"
+                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                alt="User Profile"
+              />
             </div>
             <div className="md:w-2/3 w-full px-3 flex flex-row flex-wrap">
               <div className="w-full text-center text-gray-700 font-semibold relative pt-3 md:pt-0">
