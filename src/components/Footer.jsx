936d419a36c3addf05f1091d9b397b37a39a83b1
Mitchell <mitchiemt11@gmail.com>
2023-10-31 12:52:37 +0200

    Use Router Link for navigation to external links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,33 +9,27 @@ function Footer() {
     <footer className='p-6 flex flex-col md:flex-row items-center justify-between text-white'>
       <p className='mx-3 pt-16'>Built with 💝 by Mitchell Mutandah</p>
       <div className='flex pt-16'>
-      <a
-        href='https://www.linkedin.com/in/mitchell-mutandah-5726aa212/'
-        target='_blank'>
+      <Link target='_blank' to='https://www.linkedin.com/in/mitchell-mutandah-5726aa212/'>
         <img
           className='mx-3 border-spacing-0 h-11'
           src={LINKEDIN}
           alt='LinkedIn'
         />
-      </a>
-      <a
-        href='https://dev.to/mitchiemt11'
-        target='_blank'>
+      </Link>
+      <Link target='_blank' to='https://dev.to/mitchiemt11'>
         <img
           className='mx-3 border-spacing-0 h-11'
           src={DEV}
           alt='DEV Community'
         />
-      </a>
-      <a
-        href='https://github.com/mitchiemt11'
-        target='_blank'>
+      </Link>
+      <Link target='_blank' to='https://github.com/mitchiemt11'>
         <img 
           className='mx-3 border-spacing-0 h-11'
           src={GITHUB}
           alt='Github'
         />
-      </a>
+      </Link>
       </div>
       <div className='flex pt-16'>
         <p className="mx-3">© 2023 Lekker Melodies, All Rights Reserved</p>
