4b7be584291c350baf1f46e1bda64b45958b39cf
Mitchell <mitchiemt11@gmail.com>
2023-10-31 11:56:59 +0200

    Add social links on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer className='p-6 flex flex-col md:flex-row items-center justify-between text-white'>
-      <p className='mx-3 pt-16'>Built with 💝 by <Link className='text-white hover:text-[#0274ff]' to='https://dev.to/mitchiemt11'>Mitchell Mutandah</Link> </p>
+      <p className='mx-3 pt-16'>Built with 💝 by Mitchell Mutandah</p>
+      <div className='flex'>
+      <a
+        href='https://www.linkedin.com/in/mitchell-mutandah-5726aa212/'
+        target='_blank'>
+        <img
+          className='mx-3 border-spacing-0 h-11'
+          src='https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjk4Mi1kNS0xMF8xLnBuZw.png'
+          alt='LinkedIn'
+        />
+      </a>
+      <a
+        href='https://dev.to/mitchiemt11'
+        target='_blank'>
+        <img
+          style={{border: '0px', height: '46px'}}
+          src='https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png'
+          alt='DEV Community'
+        />
+      </a>
+      <a
+        href='https://medium.com/@mitchiemt11'
+        target='_blank'>
+        <img 
+          style={{border: '0px', height: '46px'}}
+          src='https://cdn.icon-icons.com/icons2/2997/PNG/512/medium_logo_icon_187624.png'
+          border='0'
+          alt='Medium'
+        />
+      </a>
+      </div>
       <div className='flex pt-16'>
         <p className="mx-3">© 2023 Lekker Melodies, All Rights Reserved</p>
       </div>
