faf1e1a370d7be3c5642ee7fe3654cdf3e68cb83
Mitchell <mitchiemt11@gmail.com>
2025-04-03 22:46:04 +0200

    Center badge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,10 +14,12 @@ const Home = () => {
         <div className='mt-9'>
           <Cards />
         </div>
+        <div className='flex justify-center'>
 
         <a href="https://play.google.com/store/apps/details?id=com.lekkermelodies&hl=en" target="_blank" rel="noopener noreferrer">
           <img className='h-24' src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" alt="Get it on Google Play" />
         </a>
+        </div>
 
       </main>
     </div>
